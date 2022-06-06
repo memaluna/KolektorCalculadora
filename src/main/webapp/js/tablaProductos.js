896a5088ce2cd9ac0fabb6e0e70e7bf7a4015d08
Emanuel Luna <memonluna@gmail.com>
2022-06-06 18:25:04 -0300

    segundo commit, se agrega trabajo de class en tabla html

---
+++ src/main/webapp/js/tablaProductos.js
@@ -0,0 +1,12 @@
+let compras = 
+	{
+  		"compras":
+  			[  
+    			{"idProducto":"001", "nombre":"Televisor", "precio": "1000"},  
+    			{"idProducto":"002", "nombre":"camara", "precio": "100"}  
+    		]  
+}
+
+
+let contenedor = document.getElementById("contenedor");
+contenedor.innerHTML();
